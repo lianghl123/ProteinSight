78673944c5cfa7188d83b4487a17ebe890850b3f
lovelianghl <2879343819@qq.com>
2024-02-22 19:21:07 +0800

    first commit

---
+++ src/main/java/lianghl/ProteinSightApplication.java
@@ -0,0 +1,12 @@
+package lianghl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteinSightApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProteinSightApplication.class, args);
+	}
+
+}
